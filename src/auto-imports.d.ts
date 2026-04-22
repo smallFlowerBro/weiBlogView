05e427a34c93cb9a22eac0c2ef5ed1858e65a2f5
WEIYJ <1262405508@qq.com>
2026-04-22 23:51:55 +0800

    ##调整打包配置

--- src/auto-imports.d.ts
+++ src/auto-imports.d.ts
@@ -11,7 +11,6 @@ export {}
 /* prettier-ignore */
 declare module 'vue' {
   export interface GlobalComponents {
-    ElMenuItem: typeof import('element-plus/es')['ElMenuItem']
     ElSkeleton: typeof import('element-plus/es')['ElSkeleton']
     ElSkeletonItem: typeof import('element-plus/es')['ElSkeletonItem']
     RouterLink: typeof import('vue-router')['RouterLink']
--- vite.config.js
+++ vite.config.js
@@ -25,7 +25,7 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
-  //打包配置
+  //打包配置 切分太细
   build:{
     outDir   : "dist",   // 打包输出目录
     assetsDir: "assets", // 指定静态文件的存放路径
@@ -49,5 +49,39 @@ export default defineConfig({
         assetsFileNames: 'assets/[ext]/[name]-[hash].[ext]'
     }
   }
-
+    // build: {
+    //     // ⚠️ 关键：关闭 CSS 代码分割
+    //     // 这会把所有 CSS 合并成一个 style.css，而不是每个页面一个 css
+    //     cssCodeSplit: false,
+    //
+    //     rollupOptions: {
+    //         output: {
+    //             // 🚀 核心：极简分包策略
+    //             manualChunks: (id) => {
+    //                 // 1. 只要是 node_modules 里的，全部打包进 vendor.js
+    //                 if (id.includes('node_modules')) {
+    //                     return 'vendor';
+    //                 }
+    //
+    //                 // 2. 剩下的所有业务代码（views, components, utils），全部打包进 index.js
+    //                 // 不写 return 或者 return undefined，就会落入默认包
+    //                 return 'index';
+    //             },
+    //
+    //             // 📝 文件名配置
+    //             entryFileNames: 'js/[name]-[hash:8].js',
+    //             chunkFileNames: 'js/[name]-[hash:8].js',
+    //
+    //             // 🎨 资源文件归类 (保持整洁)
+    //             assetFileNames: (assetInfo) => {
+    //                 const name = assetInfo.name || '';
+    //                 let type = 'other';
+    //                 if (/\.css$/i.test(name)) type = 'css';
+    //                 else if (/\.(png|jpe?g|gif|svg|webp)$/i.test(name)) type = 'img';
+    //                 else if (/\.woff2?$|\.ttf|\.eot$/i.test(name)) type = 'font';
+    //                 return `assets/${type}/[name]-[hash:8].[ext]`;
+    //             }
+    //         }
+    //     }
+    // }
 })
